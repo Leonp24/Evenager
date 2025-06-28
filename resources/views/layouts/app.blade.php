ee041bf5de85b3d0ea9950a7c05ad169626bad21
Leonp24 <prastikomantap@gmail.com>
2025-06-28 21:38:42 +0700

    build vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,8 @@
 
     <!-- Bootstrap 5.3 CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     
 
 
