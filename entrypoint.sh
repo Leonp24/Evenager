2d217a13f3e705eeef93bcbe5825a42fed6ea798
Leonp24 <prastikomantap@gmail.com>
2025-06-28 21:24:36 +0700

    build vite

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,23 @@
 #!/bin/sh
 
-# HAPUS build sebelumnya
-rm -rf public/build
-
-# Install Laravel dependencies
+# Laravel & NPM deps
 composer install --no-interaction --prefer-dist --optimize-autoloader
 npm install
 npm run build
 
-# Set permissions
+# Rename manifest supaya Laravel bisa temukan
+mv public/build/.vite/manifest.json public/build/manifest.json
+
+# Laravel perms
 chmod -R 775 storage bootstrap/cache
 
-# Generate app key
+# Laravel setup
 php artisan key:generate
-
-# Cache Laravel
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-
-# Jalankan migrasi
 php artisan migrate --force
 
-# Start Laravel server
+# Start server
+sleep 5
 php artisan serve --host=0.0.0.0 --port=8080
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+    plugins: [laravel({
+        input: ['resources/css/app.css', 'resources/js/app.js'],
+        refresh: true,
+    })],
     build: {
-        outDir: 'public/build',
         manifest: true,
-        rollupOptions: {
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-        },
-    },
+        outDir: 'public/build',
+        emptyOutDir: true,
+    }
 });
