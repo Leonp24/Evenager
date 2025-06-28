b52ffc65ed35419710c9d7c23827664ca009d720
Leonp24 <prastikomantap@gmail.com>
2025-06-28 11:57:53 +0700

    Fix permission for entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-# Jalankan migrasi dan cache config (opsional)
-php artisan migrate --force
-php artisan config:cache
+# Install dependencies
+composer install --no-dev --optimize-autoloader
+npm install
+npm run build
 
-# Jalankan server Laravel
+# Cache konfigurasi Laravel
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Jalankan migrasi (kalau perlu)
+php artisan migrate --force
+
+# Jalankan Laravel server
+sleep 5
 php artisan serve --host=0.0.0.0 --port=8080
